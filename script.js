aede1d0355e983e53c92e22f4106ae96bd043713
Umesh Desai <117746877+UmeshDesai123@users.noreply.github.com>
2024-01-18 14:14:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count = 0;
+	library.forEach((book) => {
+		if(book.readingStatus){
+			count++;
+		}
+	});
+	return count;
 };
 
 // Do not change the code below
